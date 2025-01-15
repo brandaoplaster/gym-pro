64b1ebaf5483f29d5b026c16124a81b757a21b27
brandaoplaster <brandaoplaster@gmail.com>
2025-01-14 21:33:43 -0300

    feat: Adding field in migrate user

--- db/migrate/20250110015319_create_users.rb
+++ db/migrate/20250110015319_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false
+      t.date :birth_date, null: false
+      t.string :gender, null: false
+      t.integer :status, default: 0, null: false
 
       t.timestamps
     end
