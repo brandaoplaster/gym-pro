854588356f2d5faa1e3be7e667cddbb1ab6fecef
brandaoplaster <brandaoplaster@gmail.com>
2025-01-18 11:32:14 -0300

    feat: Adding controller exercise

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :workout_sheets
+  resources :exercises
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
