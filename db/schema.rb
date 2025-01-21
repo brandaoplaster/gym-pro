98e1ae0313d23e8aec753a0c4ee24ca5aa92c171
brandaoplaster <brandaoplaster@gmail.com>
2025-01-20 22:12:19 -0300

    feat: Update schema with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_015609) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_21_010834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_015609) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "workout_exercises", force: :cascade do |t|
