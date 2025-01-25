535073e6f1a3ae2a79a2cc871798ea7b8b2f9623
brandaoplaster <brandaoplaster@gmail.com>
2025-01-24 21:52:42 -0300

    feat: Changed null false to null true in user migrate

--- db/migrate/20250110015319_create_users.rb
+++ db/migrate/20250110015319_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.date :birth_date, null: false
-      t.string :gender, null: false
+      t.string :name, null: true
+      t.date :birth_date, null: true
+      t.string :gender, null: true
       t.integer :status, default: 0, null: false
 
       t.timestamps
