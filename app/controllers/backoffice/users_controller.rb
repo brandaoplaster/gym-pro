343c4223ec8510b87a0729aac5cf9cf7b739ebd6
brandaoplaster <brandaoplaster@gmail.com>
2025-02-13 09:35:23 -0300

    chore: Update users controller

--- app/controllers/backoffice/users_controller.rb
+++ app/controllers/backoffice/users_controller.rb
@@ -1,5 +1,5 @@
 module Backoffice
-  class UsersController < ApplicationController
+  class UsersController < BackofficeApplication
     before_action :set_user, only: %i[show edit update destroy]
 
     def index
@@ -16,9 +16,11 @@ module Backoffice
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to users_path, notice: "User was successfully created."
+        flash[:notice] = "User was successfully created."
+        redirect_to backoffice_users_path
       else
-        render :new
+        flash[:error] = "Failed to create user. Please check the form for errors."
+        render :new, status: :unprocessable_entity
       end
     end
 
