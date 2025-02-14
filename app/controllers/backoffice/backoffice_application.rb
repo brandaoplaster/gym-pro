b8bf7a45d5f38eecf9cdca75322dee33d48f1c2d
brandaoplaster <brandaoplaster@gmail.com>
2025-02-13 22:11:34 -0300

    chore: Fixing missing safe method code

--- app/controllers/backoffice/backoffice_application.rb
+++ app/controllers/backoffice/backoffice_application.rb
@@ -3,11 +3,11 @@
 module Backoffice
   class BackofficeApplication < ActionController::Base
     layout "admin"
-    before_action :authorize_admin!
+    before_action :authorize_admin
 
     private
 
-    def authorize_admin!
+    def authorize_admin
       redirect_to client_root_path, alert: "Access Denied" unless current_user.admin?
     end
   end
