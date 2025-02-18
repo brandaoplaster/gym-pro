c63f09065925517d4c2858d41231d56502a1198a
brandaoplaster <brandaoplaster@gmail.com>
2025-02-17 21:45:27 -0300

    feat: Update workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,8 @@
 
 class Workout < ApplicationRecord
   belongs_to :workout_sheet
+  has_many :exercise_sets, dependent: :destroy
+  has_many :exercises, through: :exercise_sets
+
+  accepts_nested_attributes_for :exercise_sets, allow_destroy: true
 end
