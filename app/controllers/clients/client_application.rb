bd0e094e4875e453cdcae33f5c8204ed9fc6df9f
brandaoplaster <brandaoplaster@gmail.com>
2025-02-22 22:30:35 -0300

    feat: Renamed current client method

--- app/controllers/clients/client_application.rb
+++ app/controllers/clients/client_application.rb
@@ -4,5 +4,11 @@ module Clients
   class ClientApplication < ApplicationController
     layout "client"
     before_action :authenticate_clients_client!
+
+    def current_client
+      current_clients_client
+    end
+
+    helper_method :current_client
   end
 end
